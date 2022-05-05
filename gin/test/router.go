3d765725e4c98f6eb7ac61ccb455a90eafba71e5
duanbiaowu <codean.net@gmail.com>
2022-05-05 22:05:20 +0800

    feat: gin binding data demo

--- gin/test/router.go
+++ gin/test/router.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("ping", func(c *gin.Context) {
@@ -20,6 +25,62 @@ func setupFormData() *gin.Engine {
 		name := c.Request.PostFormValue("name")
 		attrs := c.Request.PostFormValue("attrs")
 		c.String(http.StatusOK, "id = %s, name = %s, attrs = %v", id, name, attrs)
+	})
+	return r
+}
+
+func setupBindJson() *gin.Engine {
+	r := gin.Default()
+	r.POST("/login", func(c *gin.Context) {
+		var json Login
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if json.User != "admin" || json.Password != "123456" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+	return r
+}
+
+func setupBindXml() *gin.Engine {
+	r := gin.Default()
+	r.POST("/login", func(c *gin.Context) {
+		var xml Login
+		if err := c.ShouldBindXML(&xml); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if xml.User != "admin" || xml.Password != "123456" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+	return r
+}
+
+func setupBindForm() *gin.Engine {
+	r := gin.Default()
+	r.POST("/login", func(c *gin.Context) {
+		var form Login
+		// This will infer what binder to use depending on the content-type header.
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if form.User != "admin" || form.Password != "123456" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 	return r
 }
--- gin/test/router_test.go
+++ gin/test/router_test.go
@@ -28,8 +28,8 @@ func Test_setupFormData(t *testing.T) {
 	router := setupFormData()
 
 	data := url.Values{
-		"id": {"1024"},
-		"name": {"Terry"},
+		"id":    {"1024"},
+		"name":  {"Terry"},
 		"attrs": {`{height: 200, weight: 100, "hobby": ["coding", "reading"]}`},
 	}
 
@@ -40,6 +40,55 @@ func Test_setupFormData(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, `id = 1024, name = Terry, attrs = {height: 200, weight: 100, "hobby": ["coding", "reading"]}`, w.Body.String())
+}
+
+func Test_setupBindJson(t *testing.T) {
+	router := setupBindJson()
+
+	w := httptest.NewRecorder()
+	data := `{"user": "admin", "password": "123456"}`
+	req, _ := http.NewRequest("POST", "/login", strings.NewReader(data))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"status":"you are logged in"}`, w.Body.String())
+}
+
+func Test_setupBindXml(t *testing.T) {
+	router := setupBindXml()
+
+	w := httptest.NewRecorder()
+	data := `
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+	<user>admin</user>
+	<password>123456</password>
+</root>
+`
+	req, _ := http.NewRequest("POST", "/login", strings.NewReader(data))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"status":"you are logged in"}`, w.Body.String())
+}
+
+func Test_setupBindForm(t *testing.T) {
+	router := setupBindForm()
+
+	//data := url.Values{
+	//	"user": {"admin"},
+	//	"password": {"123456"},
+	//}
+	//data.Encode() = user=admin&password=123456
+
+	w := httptest.NewRecorder()
+	data := "user=admin&password=123456"
+	req, _ := http.NewRequest("POST", "/login", strings.NewReader(data))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"status":"you are logged in"}`, w.Body.String())
 }
 
 func MockRequestFile(filePath, fieldName string) (multipart.File, error) {
