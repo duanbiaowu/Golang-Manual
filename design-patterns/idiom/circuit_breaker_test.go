80d0d198f0c9f630e2fcf5fd017d1ec13f25d0df
duanbiaowu <codean.net@gmail.com>
2022-05-16 23:13:13 +0800

    feat: circuit breaker demo

---
+++ design-patterns/idiom/circuit_breaker_test.go
@@ -0,0 +1,36 @@
+package idiom
+
+import (
+	"context"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_CircuitBreaker(t *testing.T) {
+	tickets := 2
+	fn := func(ctx context.Context) error {
+		if tickets == 0 {
+			return errors.New("NoTicketsAvailable")
+		}
+		tickets--
+		return nil
+	}
+
+	ctx := context.Background()
+	circuit := Breaker(fn, 1)
+
+	err := circuit(ctx)
+	assert.Nil(t, err)
+
+	err = circuit(ctx)
+	assert.Nil(t, err)
+
+	err = circuit(ctx) // ConsecutiveFailures = 1
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New("NoTicketsAvailable"), err)
+
+	err = circuit(ctx) // ConsecutiveFailures = 1 && can not retry
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New("ErrServiceUnavailable"), err)
+}
