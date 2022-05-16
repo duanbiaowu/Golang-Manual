890bc747e39d4b20c44fdf4d300dbcdc5ff6f098
duanbiaowu <codean.net@gmail.com>
2022-05-16 10:13:17 +0800

    feat: decorator demo

--- design-patterns/structural/decorator.go
+++ design-patterns/structural/decorator.go
@@ -1,10 +1,32 @@
 package structural
 
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"time"
+)
+
 type Operator func(int) int
 
 func OpDecorator(fn Operator) Operator {
 	return func(n int) int {
 		result := fn(n)
 		return result
+	}
+}
+
+type SumFunc func(int64, int64) int64
+
+func getFuncName(fn interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+}
+
+func timedSumDecorator(f SumFunc) SumFunc {
+	return func(start int64, end int64) int64 {
+		defer func(t time.Time) {
+			fmt.Printf("--- Time Elapsed (%s): %d\n", getFuncName(f), time.Since(t))
+		}(time.Now())
+		return f(start, end)
 	}
 }
--- design-patterns/structural/decorator_test.go
+++ design-patterns/structural/decorator_test.go
@@ -5,16 +5,42 @@ import (
 	"testing"
 )
 
-func Test_OpDecorator(t *testing.T)  {
-	double := func (n int) int {
+func Test_OpDecorator(t *testing.T) {
+	double := func(n int) int {
 		return n << 1
 	}
 	d := OpDecorator(double)
 	assert.Equal(t, 10, d(5))
 
 	increment := func(n int) int {
-		return n+1
+		return n + 1
 	}
 	inc := Operator(increment)
 	assert.Equal(t, 6, inc(5))
+}
+
+func Test_TimedSumDecorator(t *testing.T) {
+	normalSum := func(start, end int64) int64 {
+		if start > end {
+			start, end = end, start
+		}
+		var sum int64 = 0
+		for i := start; i <= end; i++ {
+			sum += i
+		}
+		return sum
+	}
+
+	nSum := timedSumDecorator(normalSum)
+	assert.Equal(t, int64(500000500000), nSum(1, 1000000))
+
+	gaussSum := func(start, end int64) int64 {
+		if start > end {
+			start, end = end, start
+		}
+		return (end - start + 1) * (end + start) / 2
+	}
+
+	gSum := timedSumDecorator(gaussSum)
+	assert.Equal(t, int64(500000500000), gSum(1, 1000000))
 }
