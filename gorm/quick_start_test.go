79802019c267e444a0af5c0674e8c30efb672f59
duanbiaowu <codean.net@gmail.com>
2022-05-26 11:45:32 +0800

    refactor: gorm demo

--- gorm/quick_start_test.go
+++ gorm/quick_start_test.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
+
+	"gorm.io/gorm/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
@@ -54,8 +57,20 @@ func TestSqlite(t *testing.T) {
 }
 
 func TestMySql(t *testing.T) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+
 	dsn := "root:123456@tcp(:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		log.Println(err)
 		t.Skip()
