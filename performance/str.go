b41a6e6dfd2ef91f98c9d2a157c639a32ffe5f4f
duanbiaowu <codean.net@gmail.com>
2022-05-28 22:26:33 +0800

    feat: performance-str demo

---
+++ performance/str.go
@@ -0,0 +1,72 @@
+// See the doc: https://geektutu.com/post/hpg-string-concat.html
+
+package performance
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randomString(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func plusConcat(n int, str string) string {
+	s := ""
+	for i := 0; i < n; i++ {
+		s += str
+	}
+	return s
+}
+
+func sprintfConcat(n int, str string) string {
+	s := ""
+	for i := 0; i < n; i++ {
+		s = fmt.Sprintf("%s%s", s, str)
+	}
+	return s
+}
+
+func builderConcat(n int, str string) string {
+	var builder strings.Builder
+	for i := 0; i < n; i++ {
+		builder.WriteString(str)
+	}
+	return builder.String()
+}
+
+func bufferConcat(n int, s string) string {
+	buf := new(bytes.Buffer)
+	for i := 0; i < n; i++ {
+		buf.WriteString(s)
+	}
+	return buf.String()
+}
+
+func byteConcat(n int, str string) string {
+	buf := make([]byte, 0)
+	for i := 0; i < n; i++ {
+		buf = append(buf, str...)
+	}
+	return string(buf)
+}
+
+// 预分配性能最好
+// 最后一步类型转换时，可以直接指针转换获得更好性能
+func preByteConcat(n int, str string) string {
+	buf := make([]byte, 0, n*len(str))
+	for i := 0; i < n; i++ {
+		buf = append(buf, str...)
+	}
+	return string(buf)
+}
---
+++ performance/str_test.go
@@ -0,0 +1,31 @@
+package performance
+
+import "testing"
+
+func benchmarkStrConcat(b *testing.B, f func(int, string) string) {
+	var str = randomString(10)
+	for i := 0; i < b.N; i++ {
+		f(10000, str)
+	}
+}
+
+func BenchmarkPlusConcat(b *testing.B)    { benchmarkStrConcat(b, plusConcat) }
+func BenchmarkSprintfConcat(b *testing.B) { benchmarkStrConcat(b, sprintfConcat) }
+func BenchmarkBuilderConcat(b *testing.B) { benchmarkStrConcat(b, builderConcat) }
+func BenchmarkBufferConcat(b *testing.B)  { benchmarkStrConcat(b, bufferConcat) }
+func BenchmarkByteConcat(b *testing.B)    { benchmarkStrConcat(b, byteConcat) }
+func BenchmarkPreByteConcat(b *testing.B) { benchmarkStrConcat(b, preByteConcat) }
+
+// PreByteConcat 比 PlusConcat 快 1000+ 倍
+//BenchmarkPlusConcat
+//BenchmarkPlusConcat-8                 15          71833427 ns/op
+//BenchmarkSprintfConcat
+//BenchmarkSprintfConcat-8               8         126318812 ns/op
+//BenchmarkBuilderConcat
+//BenchmarkBuilderConcat-8            9400            114630 ns/op
+//BenchmarkBufferConcat
+//BenchmarkBufferConcat-8            10000            112820 ns/op
+//BenchmarkByteConcat
+//BenchmarkByteConcat-8               9225            122662 ns/op
+//BenchmarkPreByteConcat
+//BenchmarkPreByteConcat-8           21706             54849 ns/op
