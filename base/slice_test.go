8b1c147754a367df6ee7ee1c358c2dfe2fd42552
duanbiaowu <codean.net@gmail.com>
2022-06-04 23:03:34 +0800

    feat: slice trap demo

---
+++ base/slice_test.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+func TestSliceLockBaseArray(t *testing.T) {
+	// 切片会导致整个底层数组被锁定，底层数组无法释放内存。如果底层数组较大会对内存产生很大的压力。
+	headerMap := make(map[string][]byte)
+
+	//for i := 0; i < 5; i++ {
+	//	name := "/path/to/file"
+	//	data, err := ioutil.ReadFile(name)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	headerMap[name] = data[:1]	// 会占住 data 这个大的数组
+	//}
+
+	// 解决的方法是将结果克隆一份，这样可以释放底层的数组：
+	for i := 0; i < 5; i++ {
+		name := "/path/to/file"
+		data, err := ioutil.ReadFile(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		headerMap[name] = append([]byte{}, data[:1]...)
+	}
+}
